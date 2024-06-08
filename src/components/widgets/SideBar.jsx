aaddbe37854451845d8f705418925d44b94fe45d
nthingg <thinhnguyenngoc1424@gmail.com>
2024-06-09 06:31:22 +0700

    ThinhNN - Final Update

--- src/components/widgets/SideBar.jsx
+++ src/components/widgets/SideBar.jsx
@@ -375,17 +375,7 @@ const SideBar = () => {
             <div className="left">
               <p>Thông báo đơn hàng</p>
             </div>
-            <div className="right">
-              <Tooltip title="Đã đọc tất cả">
-                <button
-                  onClick={() => {
-                    markAllRead();
-                  }}
-                >
-                  <FactCheckIcon />
-                </button>
-              </Tooltip>
-            </div>
+            <div className="right"></div>
           </div>
         </DialogTitle>
         <DialogContent>
